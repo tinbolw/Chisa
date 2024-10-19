1b5fbc6226f838ab2999b47601651b5cbaa9dfdd
Thomas Van <36116384+tinbolw@users.noreply.github.com>
2024-10-18 21:23:21 -0700

    reformatted getslashcommands, started work on reload, added debounce package

--- commands/utility/reload.js
+++ commands/utility/reload.js
@@ -1,36 +1,45 @@
-// const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js');
+const slashcommands = require('../../lib/getslashcommands');
+const { autocomplete } = require('./autocomplete');
 
-// module.exports = {
-//   data: new SlashCommandBuilder()
-//     .setName('reload')
-//     .setDescription('Reloads a command.')
-//     .addStringOption(option =>
-//       option.setName('command')
-//         .setDescription('The command to reload.')
-//         .setRequired(true)),
-//   async execute(interaction) {
-//     if (interaction.user.id != '266413889682407428') {
-//       await interaction.editReply("You can't do this!");
-//     } else {
-//       const commandName = interaction.options.getString('command', true).toLowerCase();
-//       const command = interaction.client.commands.get(commandName);
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('reload')
+    .setDescription('Reloads a command.')
+    .addStringOption(option =>
+      option.setName('command')
+        .setDescription('The command to reload.')
+        .setAutocomplete(true)
+        .setRequired(true)),
+  async autocomplete(interaction) {
+    const query = interaction.options.getString('command');
+    const guildCommands = await slashcommands.getGuild();
+    interaction.respond([{name:'test', value:'value'}])
+    console.log(guildCommands);
 
-//       if (!command) {
-//         return interaction.editReply(`There is no command with name \`${commandName}\`!`);
-//       } else {
-//         delete require.cache[require.resolve(`./${command.data.name}.js`)];
+  },
+  async execute(interaction) {
+    if (interaction.user.id != '266413889682407428') {
+      await interaction.editReply("You can't do this!");
+    } else {
+      const commandName = interaction.options.getString('command', true).toLowerCase();
+      const command = interaction.client.commands.get(commandName);
 
-//         try {
-//           interaction.client.commands.delete(command.data.name);
-//           const newCommand = require(`./${command.data.name}.js`);
-//           interaction.client.commands.set(newCommand.data.name, newCommand);
-//           await interaction.editReply(`Command \`${newCommand.data.name}\` was reloaded!`);
-//         } catch (error) {
-//           console.error(error);
-//           await interaction.editReply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
-//         }
-//       }
-//     }
+      if (!command) {
+        return interaction.editReply(`There is no command with name \`${commandName}\`!`);
+      } else {
+        delete require.cache[require.resolve(`./${command.data.name}.js`)];
 
-//   },
-// };
+        try {
+          interaction.client.commands.delete(command.data.name);
+          const newCommand = require(`./${command.data.name}.js`);
+          interaction.client.commands.set(newCommand.data.name, newCommand);
+          await interaction.editReply(`Command \`${newCommand.data.name}\` was reloaded!`);
+        } catch (error) {
+          console.error(error);
+          await interaction.editReply(`There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``);
+        }
+      }
+    }
+  },
+};
