04eed33a5e52ea5acada6ff54b92d3bf8e5ec7e6
Thomas Van <36116384+tinbolw@users.noreply.github.com>
2024-10-20 11:22:53 -0700

    updated anilistgql functions to take type, consolidated alanimesearch and almangasearch into almediasearch, added score for both

--- commands/anilist/almediasearch.js
+++ commands/anilist/almediasearch.js
@@ -6,24 +6,32 @@ const {
   ButtonStyle,
 } = require('discord.js');
 const striptags = require('striptags');
-const { fetchAnime, searchAnime } = require('../../lib/anilistgql');
+const { fetchMedia, searchMedia } = require('../../lib/anilistgql');
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName('alanimesearch')
-    .setDescription('Search for an anime using AniList.')
+    .setName('almediasearch')
+    .setDescription('Search for media using AniList.')
     .addStringOption(option =>
-      option.setName('name')
-        .setDescription('The name of the anime')
+      option.setName('type')
+        .setDescription('The type of the media')
+        .setRequired(true)
+        .setChoices([{ name: 'anime', value: 'ANIME' }, { name: 'manga', value: 'MANGA' }]),
+    )
+    .addStringOption(option =>
+      option.setName('title')
+        .setDescription('The title of the media')
         .setRequired(true)
         .setAutocomplete(true),
     ),
   async execute(interaction) {
     var expanded = false;
-    const name = interaction.options.getString('name');
+    const mediaType = interaction.options.getString('type');
+    const mediaTitle = interaction.options.getString('title');
     const startTime = Date.now();
-    const data = await fetchAnime(name);
+    const data = await fetchMedia(mediaType, mediaTitle);
     const timeElapsed = Date.now() - startTime;
+    // initialize embed and collector
     const response = await interaction.editReply(generateReplyOptions());
     createCollector();
 
@@ -36,7 +44,7 @@ module.exports = {
         //? coverImage instead
         .setImage(data.coverImage.extraLarge)
         .setColor(data.coverImage.color)
-        .setFooter({ text: `Fetched in ${timeElapsed} ms`, iconURL: 'https://cdn.discordapp.com/attachments/975191225340686377/1297388691458625639/icon.png' });
+        .setFooter({ text: `Fetched in ${timeElapsed} ms | ${mediaType}`, iconURL: 'https://cdn.discordapp.com/attachments/975191225340686377/1297388691458625639/icon.png' });
       if (expanded) {
         let fields = [];
         //* due to the parsing requirements of nearly every field, assigning them dynamically is
@@ -44,23 +52,25 @@ module.exports = {
         fields.push({ name: "Status", value: data.status.replaceAll('_', ' '), inline: true });
         fields.push({ name: "Format", value: data.format.replaceAll('_', ' '), inline: true });
         fields.push({ name: "Popularity", value: String(data.popularity), inline: true });
-        fields.push({
-          name: `${Math.floor(data.duration / 60) == 0 ? `Episode ` : ``}Duration`, value: `${Math.floor(data.duration / 60) == 0 ? `${data.duration} min` : `${Math.floor(data.duration / 60)} hr ${Math.floor(data.duration % 60)} min`}`, inline: true
-        });
-        if (data.episodes != 1) fields.push({ name: "Episodes", value: String(data.episodes), inline: true });
+        if (data.episodes) { // parse out fields for manga, for which they do not exist
+          fields.push({
+            name: `${Math.floor(data.duration / 60) == 0 ? `Episode ` : ``}Duration`, value: `${Math.floor(data.duration / 60) == 0 ? `${data.duration} min` : `${Math.floor(data.duration / 60)} hr ${Math.floor(data.duration % 60)} min`}`, inline: true
+          });
+          if (data.episodes != 1) fields.push({ name: "Episodes", value: String(data.episodes), inline: true });
+        }
+        fields.push({ name: "Mean Score", value: String(data.meanScore), inline: true });
         fields.push({ name: "Start Date", value: `${data.startDate.month}/${data.startDate.day}/${data.startDate.year}`, inline: true });
         fields.push({ name: "End Date", value: `${data.endDate.month}/${data.endDate.day}/${data.endDate.year}`, inline: true });
         embed.setFields(fields);
       }
-      return {embeds:[embed], components:[generateButtonRow()]};
+      return { embeds: [embed], components: [generateButtonRow()] };
     }
 
     function generateButtonRow() {
       const changeEmbedStateButton = new ButtonBuilder()
         .setCustomId('changeEmbedState')
         .setStyle(ButtonStyle.Primary)
-        .setLabel(expanded?'Collapse':'Expand');
-
+        .setLabel(expanded ? 'Collapse' : 'Expand');
       return new ActionRowBuilder()
         .addComponents(changeEmbedStateButton);
     }
@@ -82,10 +92,12 @@ module.exports = {
 
   },
   async autocomplete(interaction) {
-    const name = interaction.options.getString('name');
-    const results = await searchAnime(name);
+    const mediaType = await interaction.options.getString('type');
+    const mediaTitle = await interaction.options.getString('title');
+    // catch searching for nonexistent media type
+    const results = mediaType ? await searchMedia(mediaType, mediaTitle) : ['Choose a media type first.'];
     await interaction.respond(
-      results.map(choice => ({ name: choice, value: choice })),
+      results.map(choice => ({ name: choice, value: choice }))
     );
   }
 };
