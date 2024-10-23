c3dbd8861ed57e2fd05449c2d21f702274c68d05
Thomas Van <36116384+tinbolw@users.noreply.github.com>
2024-10-23 09:59:19 -0700

    removed line breaks for uptime command output

--- commands/utility/uptime.js
+++ commands/utility/uptime.js
@@ -6,10 +6,6 @@ module.exports = {
     .setDescription('bot uptime'),
   async execute(interaction) {
     // if it works its not stupid
-    interaction.editReply(`I have been up for 
-      ${process.uptime() >= 86400 ? Math.floor(process.uptime() / 86400) + (Math.floor(process.uptime() / 86400) != 1 ? ' days ' : ' day ') : ""}
-      ${process.uptime() >= 3600 ? Math.floor((process.uptime() % 86400) / 3600) + (Math.floor((process.uptime() % 86400) / 3600) != 1 ? ' hours ' : ' hour ') : ""}
-      ${process.uptime() >= 60 ? Math.floor((process.uptime() % 86400 % 3600) / 60) + (Math.floor((process.uptime() % 86400 % 3600) / 60) != 1 ? ' minutes and ' : ' minute and ') : ""}
-      ${Math.floor(process.uptime() % 86400 % 3600 % 60)}${Math.floor(process.uptime() % 86400 % 3600 % 60) != 1 ? ' seconds' : 'second'}`);
+    interaction.editReply(`I have been up for ${process.uptime() >= 86400 ? Math.floor(process.uptime() / 86400) + (Math.floor(process.uptime() / 86400) != 1 ? ' days ' : ' day ') : ""}${process.uptime() >= 3600 ? Math.floor((process.uptime() % 86400) / 3600) + (Math.floor((process.uptime() % 86400) / 3600) != 1 ? ' hours ' : ' hour ') : ""}${process.uptime() >= 60 ? Math.floor((process.uptime() % 86400 % 3600) / 60) + (Math.floor((process.uptime() % 86400 % 3600) / 60) != 1 ? ' minutes and ' : ' minute and ') : ""}${Math.floor(process.uptime() % 86400 % 3600 % 60)}${Math.floor(process.uptime() % 86400 % 3600 % 60) != 1 ? ' seconds' : 'second'}`);
   },
 };
